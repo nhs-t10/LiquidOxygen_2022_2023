f2313f3a2ae4e403ff67855d50ca6119381b7229
jesusisfromcleveland <tml231@students.needham.k12.ma.us>
2022-11-08 19:44:34 -0500

    changed RegionBasedAveragesPipeline.java to look for rgb val in position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/CV/RegionBasedAveragesPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/CV/RegionBasedAveragesPipeline.java
@@ -27,25 +27,17 @@ public class RegionBasedAveragesPipeline extends PipelineThatExposesSomeAnalysis
     /*
      * An enum to define the skystone position
      */
-    public enum SkystonePosition
-    {
-        LEFT,
-        CENTER,
-        RIGHT
+    public enum endResult {
+        Red,
+        Green,
+        Blue
     }
 
-    /*
-     * Some color constants
-     */
-    static final Scalar BLUE = new Scalar(0, 0, 255);
-    static final Scalar GREEN = new Scalar(0, 255, 0);
 
     /*
      * The core values which define the location and size of the sample regions
      */
-    static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(109,98);
-    static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(181,98);
-    static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(253,98);
+    static final Point REGION_TOPLEFT_ANCHOR_POINT = new Point(109, 98);
     static final int REGION_WIDTH = 20;
     static final int REGION_HEIGHT = 20;
 
@@ -67,48 +59,29 @@ public class RegionBasedAveragesPipeline extends PipelineThatExposesSomeAnalysis
      *
      */
     Point region1_pointA = new Point(
-            REGION1_TOPLEFT_ANCHOR_POINT.x,
-            REGION1_TOPLEFT_ANCHOR_POINT.y);
+            REGION_TOPLEFT_ANCHOR_POINT.x,
+            REGION_TOPLEFT_ANCHOR_POINT.y);
     Point region1_pointB = new Point(
-            REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-    Point region2_pointA = new Point(
-            REGION2_TOPLEFT_ANCHOR_POINT.x,
-            REGION2_TOPLEFT_ANCHOR_POINT.y);
-    Point region2_pointB = new Point(
-            REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-    Point region3_pointA = new Point(
-            REGION3_TOPLEFT_ANCHOR_POINT.x,
-            REGION3_TOPLEFT_ANCHOR_POINT.y);
-    Point region3_pointB = new Point(
-            REGION3_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            REGION3_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+            REGION_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            REGION_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+    Scalar BLUE = new Scalar(0,0,255);
 
     /*
      * Working variables
      */
-    Mat region1_Cr, region2_Cr, region3_Cr;
-    Mat YCrCb = new Mat();
-    Mat Cr = new Mat();
+    Mat region;
     int avg1, avg2, avg3;
 
     // Volatile since accessed by OpMode thread w/o synchronization
-    private volatile SkystonePosition position = SkystonePosition.LEFT;
+    private volatile endResult result = endResult.Red;
 
     /*
      * This function takes the RGB frame, converts to YCrCb,
      * and extracts the Cb channel to the 'Cb' variable
      */
-    void inputToCr(Mat input)
-    {
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-        Core.extractChannel(YCrCb, Cr, 1);
-    }
 
     @Override
-    public void init(Mat firstFrame)
-    {
+    public void init(Mat firstFrame) {
         /*
          * We need to call this in order to make sure the 'Cb'
          * object is initialized, so that the submats we make
@@ -118,21 +91,18 @@ public class RegionBasedAveragesPipeline extends PipelineThatExposesSomeAnalysis
          * buffer would be re-allocated the first time a real frame
          * was crunched)
          */
-        inputToCr(firstFrame);
+
 
         /*
          * Submats are a persistent reference to a region of the parent
          * buffer. Any changes to the child affect the parent, and the
          * reverse also holds true.
          */
-        region1_Cr = Cr.submat(new Rect(region1_pointA, region1_pointB));
-        region2_Cr = Cr.submat(new Rect(region2_pointA, region2_pointB));
-        region3_Cr = Cr.submat(new Rect(region3_pointA, region3_pointB));
+        region = firstFrame.submat(new Rect(region1_pointA, region1_pointB));
     }
 
     @Override
-    public Mat processFrame(Mat input)
-    {
+    public Mat processFrame(Mat input) {
         /*
          * Overview of what we're doing:
          *
@@ -171,7 +141,6 @@ public class RegionBasedAveragesPipeline extends PipelineThatExposesSomeAnalysis
         /*
          * Get the Cb channel of the input frame after conversion to YCrCb
          */
-        inputToCr(input);
 
         /*
          * Compute the average pixel value of each submat region. We're
@@ -180,9 +149,9 @@ public class RegionBasedAveragesPipeline extends PipelineThatExposesSomeAnalysis
          * pixel value of the 3-channel image, and referenced the value
          * at index 2 here.
          */
-        avg1 = (int) Core.mean(region1_Cr).val[0];
-        avg2 = (int) Core.mean(region2_Cr).val[0];
-        avg3 = (int) Core.mean(region3_Cr).val[0];
+        avg1 = (int) Core.mean(region).val[0];
+        avg2 = (int) Core.mean(region).val[1];
+        avg3 = (int) Core.mean(region).val[2];
 
         /*
          * Draw a rectangle showing sample region 1 on the screen.
@@ -196,29 +165,6 @@ public class RegionBasedAveragesPipeline extends PipelineThatExposesSomeAnalysis
                 2); // Thickness of the rectangle lines
 
         /*
-         * Draw a rectangle showing sample region 2 on the screen.
-         * Simply a visual aid. Serves no functional purpose.
-         */
-        Imgproc.rectangle(
-                input, // Buffer to draw on
-                region2_pointA, // First point which defines the rectangle
-                region2_pointB, // Second point which defines the rectangle
-                BLUE, // The color the rectangle is drawn in
-                2); // Thickness of the rectangle lines
-
-        /*
-         * Draw a rectangle showing sample region 3 on the screen.
-         * Simply a visual aid. Serves no functional purpose.
-         */
-        Imgproc.rectangle(
-                input, // Buffer to draw on
-                region3_pointA, // First point which defines the rectangle
-                region3_pointB, // Second point which defines the rectangle
-                BLUE, // The color the rectangle is drawn in
-                2); // Thickness of the rectangle lines
-
-
-        /*
          * Find the max of the 3 averages
          */
         int maxOneTwo = Math.min(avg1, avg2);
@@ -228,60 +174,38 @@ public class RegionBasedAveragesPipeline extends PipelineThatExposesSomeAnalysis
          * Now that we found the max, we actually need to go and
          * figure out which sample region that value was from
          */
-        if(max == avg1) // Was it from region 1?
+        if (max == avg1) // Was it from region 1?
         {
-            position = SkystonePosition.LEFT; // Record our analysis
+            result = endResult.Blue; // Record our analysis
 
             /*
              * Draw a solid rectangle on top of the chosen region.
              * Simply a visual aid. Serves no functional purpose.
              */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region1_pointA, // First point which defines the rectangle
-                    region1_pointB, // Second point which defines the rectangle
-                    GREEN, // The color the rectangle is drawn in
-                    -1); // Negative thickness means solid fill
-        }
-        else if(max == avg2) // Was it from region 2?
+        } else if (max == avg2) // Was it from region 2?
         {
-            position = SkystonePosition.CENTER; // Record our analysis
+            result = endResult.Green; // Record our analysis
 
             /*
              * Draw a solid rectangle on top of the chosen region.
              * Simply a visual aid. Serves no functional purpose.
              */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region2_pointA, // First point which defines the rectangle
-                    region2_pointB, // Second point which defines the rectangle
-                    GREEN, // The color the rectangle is drawn in
-                    -1); // Negative thickness means solid fill
-        }
-        else if(max == avg3) // Was it from region 3?
+        } else if (max == avg3) // Was it from region 3?
         {
-            position = SkystonePosition.RIGHT; // Record our analysis
+            result = endResult.Red; // Record our analysis
+
+
 
             /*
-             * Draw a solid rectangle on top of the chosen region.
-             * Simply a visual aid. Serves no functional purpose.
+             * Render the 'input' buffer to the viewport. But note this is not
+             * simply rendering the raw camera feed, because we called functions
+             * to add some annotations to this buffer earlier up.
              */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region3_pointA, // First point which defines the rectangle
-                    region3_pointB, // Second point which defines the rectangle
-                    GREEN, // The color the rectangle is drawn in
-                    -1); // Negative thickness means solid fill
+            return input;
         }
-
-        /*
-         * Render the 'input' buffer to the viewport. But note this is not
-         * simply rendering the raw camera feed, because we called functions
-         * to add some annotations to this buffer earlier up.
-         */
         return input;
     }
-    public int getAnalysis() {
-        return position.ordinal();
+        public int getAnalysis () {
+            return result.ordinal();
+        }
     }
-}
