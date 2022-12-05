203f7a9ceffb70339cbea005491f092e6b6f76b8
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2022-12-05 15:54:34 -0500

    Add robot measurements

--- TeamCode/src/main/java/liquidoxygen/Shared.java
+++ TeamCode/src/main/java/liquidoxygen/Shared.java
@@ -8,13 +8,13 @@ public class Shared {
 	public static final String TELEOP_NAME = "Example TeleOp Mode";
 	public static final String AUTONOMOUS_NAME = "Example Autonomous Mode";
 	public static final String GROUP = "lo2";
-	public static final Robot ROBOT = new Robot(40d, 60d, 50d, 9326, "Robot", true);
+	public static final Robot ROBOT = new Robot(43, 29, 27, 9326, "Robot", true);
 
 	public static CarWheels createWheels(HardwareMap hardwareMap) {
 		return new CarWheels(
 			hardwareMap,
 			1120,
-			1000, // TODO: measure the actual diameter (in cm) of the wheels
+			10,
 			ROBOT,
 			"Front Left Motor",
 			"Front Right Motor",
