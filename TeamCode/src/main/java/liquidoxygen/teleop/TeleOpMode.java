ff0f7a0b78859bf136efd1cb105f1e11daa294f0
jesusisfromcleveland <tml231@students.needham.k12.ma.us>
2023-01-20 08:29:19 -0500

    Quick update

--- TeamCode/src/main/java/liquidoxygen/teleop/TeleOpMode.java
+++ TeamCode/src/main/java/liquidoxygen/teleop/TeleOpMode.java
@@ -5,18 +5,23 @@ import com.pocolifo.robobase.control.GamepadCarWheels;
 import com.pocolifo.robobase.motor.Wheel;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import liquidoxygen.Shared;
 
 @TeleOp(name = Shared.TELEOP_NAME, group = Shared.GROUP)
 public class TeleOpMode extends TeleOpOpMode {
 	private GamepadCarWheels wheels;
 	private Wheel liftMotor;
+	private Servo grabberServo;
+	private boolean isGrabbing;
+	private boolean aWasDownLast;
 
 	@Override
 	public void initialize() {
 		this.wheels = new GamepadCarWheels(Shared.createWheels(this.hardwareMap), this.gamepad1);
 		this.liftMotor = new Wheel(this.hardwareMap.dcMotor.get("Lift"), 288, 5.5);
 		this.liftMotor.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		this.grabberServo = this.hardwareMap.servo.get("Grabber");
 
 		System.out.println("Successfully initialized.");
 	}
@@ -35,6 +40,19 @@ public class TeleOpMode extends TeleOpOpMode {
 		} else {
 			this.liftMotor.drive(0);
 		}
+
+		// Grabber
+		if (this.gamepad1.a && this.aWasDownLast) {
+			if (this.isGrabbing) {
+				this.grabberServo.setPosition(1);
+			} else {
+				this.grabberServo.setPosition(0);
+			}
+
+			this.isGrabbing = !this.isGrabbing;
+		}
+
+		this.aWasDownLast = this.gamepad1.a;
 	}
 
 	@Override
